2f75af11e9a95731ae80ca085e7622278126b088
ZahrahAhmed <zm99__@hotmail.com>
2017-11-06 20:59:42 +0300

    linked list to detail page

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Post(models.Model):
 	topic = models.CharField(max_length=100)
@@ -8,3 +9,6 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.topic
+
+	def get_detail_url(self):
+		return reverse("detail", kwargs={"post_id": self.id })
